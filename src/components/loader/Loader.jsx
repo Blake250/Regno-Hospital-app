7fdb12c30336c95915e9ac14fccf51aa31761005
Blake250 <ozoekwecelestine@gmail.com>
2025-07-23 15:50:15 +0100

    pushing fi

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,47 +1,90 @@
 
 
 
-import { Box } from "@mui/material"
-//import { useSelector } from "react-redux"
+import { Box } from "@mui/material";
 
 const Loader = () => {
-
-  //  const {isLoading} = useSelector((state)=>state?.auth)
   return (
     <Box
-    sx={{
-      width:'100%',
-      height:'100%',
-      backgroundColor:' rgba(0, 0, 0, 0.7)!important' ,
-      zIndex: '9',
-    position: 'fixed'
-    }}
+      sx={{
+        width: '100%',
+        height: '100vh', // Full viewport height
+        backgroundColor: 'rgba(0, 0, 0, 0.7)',
+        zIndex: 9999,
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        display: 'flex',           // <== move flex properties here
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
     >
-        <Box
-        sx={{
-          display:'flex',
-          justifyContent:'center',
-          alignItems:'center',
-      //    height:'100vh',
-          zIndex:'999'
+      <img
+        style={{
+          width: '15%',
         }}
+        src="./spinner/spinner.jpg"
+        alt="Loading..."
+      />
+    </Box>
+  );
+};
+
+export default Loader;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Box } from "@mui/material"
+// //import { useSelector } from "react-redux"
+
+// const Loader = () => {
+
+//   //  const {isLoading} = useSelector((state)=>state?.auth)
+//   return (
+//     <Box
+//     sx={{
+//       width:'100%',
+//       height:'100%',
+//       backgroundColor:' rgba(0, 0, 0, 0.7)!important' ,
+//       zIndex: '9',
+//     position: 'fixed'
+//     }}
+//     >
+//         <Box
+//         sx={{
+//           display:'flex',
+//           justifyContent:'center',
+//           alignItems:'center',
+//       //    height:'100vh',
+//           zIndex:'999'
+//         }}
   
-        >
-     <img 
-     style={{
-      width:'15%',
+//         >
+//      <img 
+//      style={{
+//       width:'15%',
 
 
-     }}
-     src="./spinner/spinner.jpg" alt="" />
-        </Box>
+//      }}
+//      src="./spinner/spinner.jpg" alt="" />
+//         </Box>
        
         
-    </Box>
+//     </Box>
 
-  )
-}
+//   )
+// }
 
-export default Loader
+// export default Loader
 
 
