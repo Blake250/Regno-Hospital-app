de76dc79ffce032326950ec37dd9a16d0d4db873
Blake250 <ozoekwecelestine@gmail.com>
2025-07-31 09:08:59 +0100

    pushing fix

--- src/feature/adminAuth/adminService.jsx
+++ src/feature/adminAuth/adminService.jsx
@@ -7,7 +7,7 @@ import axios from 'axios';
 
 // ADD  doctor as an admin
 export const addDoctor = async(doctorData )=>{
-//    const response = await axios.post(`${API_URL}/add-doc`,doctorData,{
+
  const response = await axios.post(`'https://regno-hospital-api.onrender.com/api/admin'/add-doc`,doctorData,{
     withCredentials:true,
     headers:{'Content-Type':'application/json'}
@@ -24,7 +24,7 @@ export const addDoctor = async(doctorData )=>{
 //update  a doctor's photo  
 export const doctorPhoto = async( docPhoto)=>{
     const response = await axios.patch(`https://regno-hospital-api.onrender.com/api/admin/doc-photo`, docPhoto, {
-     //     const response = await axios.patch(`${API_URL}/doc-photo`, docPhoto, {
+   
         withCredentials: true,
       //  headers: {'Content-Type': 'application/json'},
         headers: {'Content-Type': 'multipart/form-data'}    
@@ -37,7 +37,7 @@ export const doctorPhoto = async( docPhoto)=>{
 // fetch all docs by admin
 export const getAllDocsByAdmin = async()=>{
     const response = await axios.get(`https://regno-hospital-api.onrender.com/api/admin/get-admin-docs`, {
-     ///    const response = await axios.get(`${API_URL}/get-admin-docs`, {
+     
         withCredentials: true,
         headers: {'Content-Type': 'application/json'}
     });
@@ -55,7 +55,7 @@ export const getAllDocsByAdmin = async()=>{
 // get all appointments as an admin
 export const appointmentAdmin = async()=>{
     const response = await axios.get(`https://regno-hospital-api.onrender.com/api/admin/get-booking`, {
-      //  const response = await axios.get(`${API_URL}/get-booking`, {
+     
         withCredentials: true,
         headers: {'Content-Type': 'application/json'}
     });
@@ -69,7 +69,7 @@ export const appointmentAdmin = async()=>{
 // fetch all cancel appointments as an admin 
 export const appointmentCancel = async()=>{
     const response = await axios.get(`https://regno-hospital-api.onrender.com/api/admin/cancelled-appointments`, {
-       //   const response = await axios.get(`${API_URL}/cancelled-appointments`, {
+    
         withCredentials: true,
         headers: {'Content-Type': 'application/json'}
     });
