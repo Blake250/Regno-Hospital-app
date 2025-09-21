14392753944deb409afb3d9b4a3365a2805e59e0
Christian <89995961+Blake250@users.noreply.github.com>
2025-09-21 13:44:58 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -130,11 +130,11 @@ const shouldHideFooter =
       <Box sx={{ flexGrow: 1 }}>
         <Routes>
       
-           <Route path="/home" element={ <Home/>} />
+           <Route path="/" element={ <Home/>} />
           <Route path="/doctors" element={ <Doctors/>} />
           <Route path="/doctors/:speciality" element={<Doctors />} />
           <Route path="/register" element={  <Register />} />
-          <Route path="/" element={   isLoggedIn   && user ? <Navigate to="/home" /> : <Login />} />
+          <Route path="/login" element={   isLoggedIn   && user ? <Navigate to="/" /> : <Login />} />
           
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
