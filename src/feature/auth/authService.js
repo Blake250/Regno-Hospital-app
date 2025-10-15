f5c6d6c791065824b41c49c50425f1abaa6c05fc
Blake250 <ozoekwecelestine@gmail.com>
2025-10-15 17:01:17 +0100

    pushing src/feature/auth/authService.js

--- src/feature/auth/authService.js
+++ src/feature/auth/authService.js
@@ -243,7 +243,7 @@ const authService = {
 
 
 
-import axios from "axios";
+//import axios from "axios";
 
 // const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 // const API_URL = `${BACKEND_URL}/api/user`;
